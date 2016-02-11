050f8c067aa7b8998d41c9c3a8bf80713115b7db
lhannest <lhannest@gmail.com>
2016-02-11 08:21:41 -0800

    Added mnist dataset, and an example using it. Low success rate, around 65-70% on a small subset of the data. Also added a printer class for more attractive outputs during the learning phase.

---
+++ src/printer.py
@@ -0,0 +1,20 @@
+import sys
+import time
+
+class printer(object):
+	def __init__(self, wait_time):
+		self.wait_time = wait_time
+		self.t = time.time()
+		self.last_length = 0
+		
+	def overwrite(self, message='', wait=True):
+		if time.time() - self.t >= self.wait_time or not wait:
+			sys.stdout.write(' '*self.last_length + '\r')
+			sys.stdout.flush()
+			sys.stdout.write(message)
+			sys.stdout.flush()
+			self.t = time.time()
+			self.last_length = len(message)
+
+	def clear(self):
+		self.overwrite(wait=False)
