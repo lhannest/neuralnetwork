e8f2c23f7b97c96b65f648a6782e899718a6a12b
lhannest <lhannest@gmail.com>
2016-03-04 08:36:48 -0800

    Added the capacity to choose between squaredError and crossEntropyError functions in nnet.learn()

--- src/neuralnetwork.py
+++ src/neuralnetwork.py
@@ -2,21 +2,36 @@ import numpy as np
 import os.path
 import pudb
 
+# np.seterr(all='raise')
+
 def sigmoid(x):
-	y = 1 / (1 + np.exp(-x))
+	exp = np.nan_to_num(np.exp(-x))
+	y = (1 / (1 + exp))
 	return y, y * (1 - y)
 
 def softplus(x):
-	return np.log(1 + np.exp(x)), 1 / (1 + np.exp(-x))
+	exp = np.nan_to_num(np.exp(-x))
+	return np.log(1 + exp), 1 / (1 + exp)
 
 def relu(x):
-	return np.maximum(0, x), (x > 0) * 1
+	x = np.nan_to_num(x)
+	return np.maximum(0, x), (x >= 0) * 1
 
-def getSquaredError(errors):
-	squared_error = 0
-	for error in errors:
-		squared_error += error**2
-	return squared_error / 2
+def squaredError(layer, targets, isDerivative=False):
+	if isDerivative:
+		return (layer.outputs - np.array(targets))*layer.derivative
+	else:
+		return 0.5*np.sum((layer.outputs - np.array(targets))**2)
+
+def crossEntropyError(layer, targets, isDerivative=False):
+	if isDerivative:
+		return layer.outputs - np.array(targets)
+	else:
+		np.sum(np.nan_to_num(-y*np.log(a)-(1-y)*np.log(1-a)))
+		t = np.array(targets)
+		y = np.array(layer.outputs)
+		return -np.sum(np.nan_to_num(t*np.lon(y) + (1 - t)*np.log(1 - y)))
+
 
 def save(neuralnetwork, fileName, overwrite=False):
 	weights = []
@@ -38,21 +53,20 @@ def makeNewName(fileName, number):
 def load(fileName):
 	data = np.load(fileName + ".npy")
 	weights = data[0]
-	shape = data[1]
 
 	layers = []
 	for weight in weights:
 		layers.append(Layer(0,0))
 		layers[-1].weights = weight
-	neuralnetwork = NeuralNetwork([])
+	neuralnetwork = NeuralNetwork()
 	neuralnetwork.layers = layers
-	neuralnetwork.shape = shape
 	return neuralnetwork
 
 class Layer(object):
-	def __init__(self, input_size, output_size, f=sigmoid, magnitude=0):
+	def __init__(self, input_size, output_size, fn=sigmoid, magnitude=0):
 		self.weights = np.random.randn(input_size + 1, output_size) / 10**magnitude
-		self.activation_function = f
+		self.activation_function = fn
+		self.shape = (input_size, output_size)
 
 	def feedforward(self, inputs):
 		self.inputs = np.append(inputs, 1)
@@ -60,38 +74,38 @@ class Layer(object):
 		self.outputs, self.derivative = self.activation_function(z)
 		return self.outputs
 
-	def learn(self, inputs, targets, step_size=1):
-		outputs = self.feedforward(inputs)
-		errors = (outputs - np.array(targets)) * self.derivative
-		self.weights -= step_size * np.outer(self.inputs, errors)
-
 	def backpropagate(self, errors):
 		self.errors = errors * self.derivative
 		return np.dot(self.weights, self.errors)[:-1]
 
 	def updateWeights(self, step_size=1):
-		self.weights -= step_size * np.outer(self.inputs, self.errors)
+		dweight = np.outer(self.inputs, self.errors)
+		A = np.linalg.norm(dweight)
+		B = np.linalg.norm(self.weights)
+		self.weights -= step_size * dweight
 
 class NeuralNetwork(object):
-	def __init__(self, layer_sizes, magnitude=0):
+	def __init__(self, layer_sizes=[], magnitude=0):
 		# If new attributes are ever added, ensure to update save() and load()
 		self.layers = [Layer(x, y, magnitude=magnitude) for x, y in zip(layer_sizes[:-1], layer_sizes[1:])]
-		self.shape = tuple(layer_sizes)
+
+	@property
+	def shape(self):
+		return tuple([l.shape[1] for l in self.layers])
 
 	def evaluate(self, inputs):
 		for layer in self.layers:
 			inputs = layer.feedforward(inputs)
 		return inputs
 
-	def learn(self, inputs, targets, step_size=1):
+	def learn(self, inputs, targets, step_size=1, errfn=crossEntropyError):
 		outputs = self.evaluate(inputs)
-		errors = outputs - np.array(targets)
-		squared_error = getSquaredError(errors)
 
-		for layer in reversed(self.layers):
+		self.layers[-1].errors = errfn(self.layers[-1], targets, isDerivative=True)
+		errors = np.dot(self.layers[-1].weights, self.layers[-1].errors)[:-1]
+
+		for layer in reversed(self.layers[:-1]):
 			errors = layer.backpropagate(errors)
 
 		for layer in self.layers:
 			layer.updateWeights(step_size)
-
-		return squared_error
