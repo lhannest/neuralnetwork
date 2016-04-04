7412e9695a8569794d47afa56eb64a879567b008
lhannest <lhannest@gmail.com>
2016-04-04 16:07:43 -0700

    Fixed glitch with printer.py

--- src/printer.py
+++ src/printer.py
@@ -1,15 +1,16 @@
 import sys
 import time
+import pudb
 
 class Printer(object):
 	def __init__(self, wait_time):
 		self.wait_time = wait_time
 		self.t = time.time()
 		self.last_length = 0
-		
+
 	def overwrite(self, message='', wait=True):
 		if time.time() - self.t >= self.wait_time or not wait:
-			sys.stdout.write(' '*self.last_length + '\r')
+			sys.stdout.write('\r' + ' '*self.last_length + '\r')
 			sys.stdout.flush()
 			sys.stdout.write(message)
 			sys.stdout.flush()
